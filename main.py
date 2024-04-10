7e6eb9761197cc2b1b679096a5692b76a4264ffe
Md. Nafiu Rahman <92587397+100100guy@users.noreply.github.com>
2024-04-10 15:28:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ import numpy as np
 import joblib
 from pipeline_ import predict_survival
 
-# Define the title and description of the web app
 st.title('Titanic Survival Prediction')
 st.write('This is a simple web app to predict the survival of passengers on the Titanic.')
-
-# Create a form to enter passenger details
 st.header('Passenger Details')
 
-# Collect input features - PassengerId, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked
 passenger_id = st.number_input('Passenger ID', min_value=0)
 pclass = st.selectbox('Passenger Class', ['1', '2', '3'])
 name = st.text_input('Name')
 
-# select gender
 sex = st.selectbox('Sex',["male","female"])
 
 age = st.number_input('Age', min_value=0)
@@ -27,18 +22,16 @@ fare = st.number_input('Fare', min_value=0.0)
 cabin = st.text_input('Cabin')
 embarked = st.selectbox('Embarked', ['C', 'Q', 'S','N'])
 
-# Create a DataFrame with the input features
 data = pd.DataFrame({'PassengerId': passenger_id, 'Pclass': pclass , 'Name': name,
                      'Sex':sex, 'Age': age, 'SibSp': sibsp, 'Parch': parch,
                         'Ticket': ticket, 'Fare': fare, 'Cabin': cabin, 'Embarked': embarked}, index=[0])
 prediction = 0
-# a button to submit the form
+
 if st.button('Predict Survival'):
     print(data)
     prediction = predict_survival(data)
     print(prediction)
 
-# Display the prediction
 st.header('Prediction')
 if prediction == 1:
     st.write('The passenger is likely to survive.')
